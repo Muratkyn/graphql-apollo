7d3b97f8d8145d369ae29e28943198c04237a1ec
Murat Koyun <muratk1845@gmail.com>
2024-12-05 08:57:23 +0100

    added queries & resolvers for related data

--- index.js
+++ index.js
@@ -9,11 +9,38 @@ const resolvers = {
     games() {
       return games;
     },
+    game(_, args) {
+      return games.find((game) => game.id === args.id);
+    },
     authors() {
       return authors;
     },
+    author(_, args) {
+      return authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return reviews;
+    },
+    review(_, args) {
+      return reviews.find((rev) => rev.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Reviews: {
+    game(parent) {
+      return games.filter((game) => game.id === parent.game_id);
+    },
+    author(parent) {
+      return authors.filter((author) => author.id === parent.author_id);
     },
   },
 };
