42383251fef63af9725a54e4a1e93151377b5878
Murat Koyun <muratk1845@gmail.com>
2024-12-05 15:27:12 +0100

    fix: refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 import { games, reviews, authors } from "./db.js";
-import { typeDefs } from "./schema.js";
+import { typeDefs } from "../schema.js";
 
 const resolvers = {
   Query: {
